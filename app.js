07a41c1a0c9f0e3848ebd36d25b1c33f251ad7a2
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-11-07 22:16:02 +0200

    updated the javascript file

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
-//function for refreshing the feed page
+let feedContainerElement = document.querySelector("feed-container");
+
+// Function for refreshing the feed page
 document.getElementById("logo-container").addEventListener("click", () => {
-  window.location.reload();
+  let linkedinFeedUrl =
+    "https://www.linkedin.com/feed/update/urn:li:activity:7219434359085252608";
+  getFeed(linkedinFeedUrl);
 });
+
+// getFeed function for homepage
+function getFeed(url) {
+  let apiUrl = "https://linkedin-api8.p.rapidapi.com/get-post";
+  const options = {
+    params: {
+      url: url, // Pass LinkedIn URL as a parameter
+    },
+    headers: {
+      "x-rapidapi-key": "eea6a34f4dmsh296963a5654d19ep13d93ajsn94fa453c501f",
+      "x-rapidapi-host": "linkedin-api8.p.rapidapi.com",
+    },
+  };
+  axios
+    .get(apiUrl, options)
+    .then(renderFeed)
+    .catch((error) => console.error(error));
+}
+
+// Function for rendering feed
+function renderFeed(response) {
+  console.log(response.data);
+}
